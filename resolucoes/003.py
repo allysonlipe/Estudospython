08f3678cd8b5de2aea95aacb41ea45575e513fed
allysonlipe <allysongunner1@hotmail.com>
2024-07-28 22:10:42 -0300

    resolucao ate Q03

---
+++ resolucoes/003.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+n1 = input('Digite um Número: ')
+
+n2 = input('Digite outro Número: ')
+
+soma = int(n1) + int(n2)
+
+print('A soma entre: '+ str(n1) +' e ' + str(n2) +' é: '+ str(soma))
